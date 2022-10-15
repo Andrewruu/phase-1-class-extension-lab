ad2c0fb42b37036b2a285ce959c8fb15c261610c
Andrew Wang <Andrewruujin@gmail.com>
2022-10-15 14:40:12 -0400

    commit tree 85a47dc85c6f4d3c79917dc5a72e68f48c96aec3

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+class Polygon{
+    constructor(arraySides){
+        this.sides = arraySides
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        let total = 0
+        for (let i of this.sides){
+            total = total + i
+        }
+        return total
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        
+        if (this.countSides !== 3)return;
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if (this.countSides !== 4)
+        {
+            return;
+        }
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        let side4 = this.sides[3]
+
+        return side1==side2 && side1==side3 && side1==side4
+    }
+
+    get area(){
+        if (this.countSides !== 4)
+        {
+            return;
+        }
+        return this.sides[0]*this.sides[0]
+    }
+}
+
